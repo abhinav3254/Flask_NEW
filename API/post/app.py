51f782f5c937f7322c2017247e33f59f84b28536
abhinav3254 <abhinav3254@gmail.com>
2022-03-09 19:15:46 +0530

    PUT and DELETE

--- API/post/app.py
+++ API/post/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask,Request,jsonify, request
 
 
@@ -46,7 +47,32 @@ def books():
         books_list.append(new_obj)
         return jsonify(books_list)
 
-@app.route('/books/<int:id>')
+@app.route('/books/<int:id>',methods=['GET','PUT','DELETE'])
+def single_book(id):
+    if request.method == 'GET':
+        for book in books_list:
+            if book['id'] == id:
+                return jsonify(book)
+    if request.method == 'PUT':
+        for book in books_list:
+            if book['id'] == id:
+                book['name'] = request.form['name']
+                book['pages'] = request.form['pages']
+
+                update_book = {
+                    'name':book['name'],
+                    'pages':book['pages'],
+                    'id':id
+                }
+
+                return jsonify(update_book)
+
+    if request.method == 'DELETE':
+        for index , book in enumerate(books_list):
+            if book['id'] == id:
+                books_list.pop(index)
+                return jsonify(books_list)
+
 
 if __name__ == '__main__':
     app.run()

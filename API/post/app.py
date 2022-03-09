06d2299521d3ebe92a1fdb8edda3cc729f65fe68
abhinav3254 <abhinav3254@gmail.com>
2022-03-09 18:51:07 +0530

    Adding ID

--- API/post/app.py
+++ API/post/app.py
@@ -6,19 +6,24 @@ app = Flask(__name__)
 books_list = [
     {
         'name':'java',
-        'pages':'150'
+        'pages':'150',
+        'id':0
     },
     {
         'name':'python',
-        'pages':'100'
+        'pages':'100',
+        'id':1
+
     },
     {
         'name':'c',
-        'pages':'10'
+        'pages':'10',
+        'id':2
     },
     {
         'name':'c++',
-        'pages':'70'
+        'pages':'70',
+        'id':3
     }
 ]
 
@@ -31,13 +36,17 @@ def books():
     if request.method == 'POST':
         new_name = request.form['name']
         new_pages = request.form['pages']
+        iD = books_list[-1]['id']+1
 
         new_obj = {
             'name':new_name,
-            'pages':new_pages
+            'pages':new_pages,
+            'id':iD
         }
         books_list.append(new_obj)
         return jsonify(books_list)
+
+@app.route('/books/<int:id>')
 
 if __name__ == '__main__':
     app.run()
